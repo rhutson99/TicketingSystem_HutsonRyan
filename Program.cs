391fc3706435c67091a5a0527f86ff35e5bd70bc
ryanhutson99 <ryanhutson99@gmail.com>
2021-03-08 09:46:43 -0600

    added menu for dif types and enchancement input

--- Program.cs
+++ Program.cs
@@ -9,10 +9,14 @@ namespace TicketingSystem
     {
         static void Main(string[] args)
         {
-            string file = "Tickets.csv";
+            string file1 = "Tickets.csv";
+            string file2 = "Enhancements.csv";
+            string file3 = "Task.csv";
             string input;
 
-            SystemFile systemFile = new SystemFile(file);
+            SystemFile systemFile1 = new SystemFile(file1);
+            SystemFile systemFile2 = new SystemFile(file2);
+            SystemFile systemFile3 = new SystemFile(file3);
 
             //ask user input menu
             do{
@@ -24,7 +28,15 @@ namespace TicketingSystem
 
             if(input == "1")
             {
-                if (File.Exists(file))
+                //user menu for choosing what kind of ticket to create
+                Console.WriteLine("1) Bug or Defect.");
+                Console.WriteLine("2) Enhancement.");
+                Console.WriteLine("3) Task");
+
+                input = Console.ReadLine();
+
+                //ticket system for bugs and defects
+                if(input == "1")
                 {
                     BugDefect ticket = new BugDefect();
                 //create ticket
@@ -36,7 +48,7 @@ namespace TicketingSystem
                 ticket.status = Console.ReadLine();
                 Console.WriteLine("Please input a priority.");
                 ticket.priority = Console.ReadLine();
-                Console.WriteLine("Please input a Submitter.");
+                Console.WriteLine("Please input a submitter.");
                 ticket.submitter = Console.ReadLine();
                 Console.WriteLine("Please input the person assigned.");
                 ticket.assigned = Console.ReadLine();
@@ -59,9 +71,52 @@ namespace TicketingSystem
                 
                 
                 SystemFile.AddTicket(ticket);
-                
-
                 }
+
+                //ticketing system for enhancements
+                if(input == "2")
+                {
+                    Enchancement ticket = new Enchancement();
+                //create ticket
+                Console.WriteLine("Please input a ticket ID.");
+                ticket.id = Console.ReadLine();
+                Console.WriteLine("Please input a ticket summary.");
+                ticket.summary = Console.ReadLine();
+                Console.WriteLine("Please input a status.");
+                ticket.status = Console.ReadLine();
+                Console.WriteLine("Please input a priority.");
+                ticket.priority = Console.ReadLine();
+                Console.WriteLine("Please input a submitter.");
+                ticket.submitter = Console.ReadLine();
+                Console.WriteLine("Please input the person assigned.");
+                ticket.assigned = Console.ReadLine();
+                Console.WriteLine("Please input a watcher.");
+                
+                 //multiple inputs for watcher on one ticket
+
+                int i;
+                for(i=0; i < 3; i++)
+                {
+                    string watch = Console.ReadLine();
+                    ticket.watchers.Add(watch);
+                    Console.WriteLine("Would you like to add another watcher? (Y/N)");
+                    string res = Console.ReadLine().ToUpper();
+                    if (res != "Y") {break;}
+                }
+
+                Console.WriteLine("Please input a software.");
+                ticket.software = Console.ReadLine();
+                Console.WriteLine("Please input a cost.");
+                ticket.cost = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please input a reason.");
+                ticket.reason = Console.ReadLine();
+                Console.WriteLine("Please input an estimate.");
+                ticket.estimate = Console.ReadLine();
+                
+                
+                SystemFile.AddTicket(ticket);
+                }
+
 
 
 
